cd15dfe7c1b2569267c0321aeb0761bdbf953b44
julio <julio@maxxitani.id>
2022-06-28 16:02:43 +0700

    sample

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { Star } from './Star';
-import { Order } from './Order';
 
 function App() {
   return (
     <div className="App">
-      <Star/>
-      <div className='line'></div>
-      <Order data={['A', 'B', 'C']}/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
